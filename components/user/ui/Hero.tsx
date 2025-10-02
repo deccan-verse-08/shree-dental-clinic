d929ea2af6e67b2ce00b7fdd0c266ce544195ea1
Anup Tiwari <anooptiwari4766@gmail.com>
2025-10-02 12:47:48 -0700

    Simple Hero

--- components/user/ui/Hero.tsx
+++ components/user/ui/Hero.tsx
@@ -3,6 +3,9 @@
 const Hero=()=>{
     return(
         <div className="h-200 w-full relative bg-[--background]">
+            <div className="flex items-center justify-center h-full">
+                <h1 className="text-9xl font-bold">Shree Dental Clinic</h1>
+            </div>
 
         </div>
     )
